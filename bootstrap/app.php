77434bbcbff9f5bec200dbda6204ea641c892ea1
draganb24 <nemanjabjelica45@gmail.com>
2024-04-22 00:43:22 +0200

    integrated spatie image optimizer

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,8 +7,8 @@ use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
         using: function () {
             Route::middleware('api')
@@ -20,7 +20,9 @@ return Application::configure(basePath: dirname(__DIR__))
         },
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        return [
+            'optimizeImages' => \Spatie\LaravelImageOptimizer\Middlewares\OptimizeImages::class,
+        ];
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
