28eb36588efd72ea701030538393bf4161ba57ad
draganb24 <nemanjabjelica45@gmail.com>
2024-04-24 21:19:37 +0200

    Resized uploaded image

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Image;
-use App\Models\TemporaryFile;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Laravel\Facades\Image as ImageIntervention;
+
 
 class UploadController extends Controller
 {
@@ -12,16 +14,27 @@ class UploadController extends Controller
     {
         if ($request->hasFile('image')) {
             $file = $request->file('image');
+            $fileImage = ImageIntervention::read($file);
+            $fileImage->resize(400, 400);
+
             $filename = $file->getClientOriginalName();
             $folder = uniqid() . '-' . now()->timestamp;
-            $path = $file->storeAs('public/images/tmp/' . $folder, $filename);
+            $path = 'public/images/tmp/' . $folder . '/' . $filename;
+
+            Storage::makeDirectory(dirname($path));
+
+            $fileImage->save(storage_path('app/' . $path));
+
+            $fileImage->save(storage_path('app/' . $path));
 
             $image = new Image();
             $image->image_caption = $filename;
             $image->image_path = str_replace('public/', '', $path);
             $image->save();
+
             return $image->id;
         }
+
         return '';
     }
 }
