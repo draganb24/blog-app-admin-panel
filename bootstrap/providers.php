f18c96a9786648536b90d137f58a61ef73a10581
draganb24 <nemanjabjelica45@gmail.com>
2024-04-24 19:03:03 +0200

    implemented intervention image

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,7 +3,6 @@
 return [
     App\Providers\AliasServiceProvider::class,
     App\Providers\AppServiceProvider::class,
-    Intervention\Image\ImageServiceProvider::class,
     Unisharp\Laravelfilemanager\LaravelFilemanagerServiceProvider::class,
     App\Providers\AliasServiceProvider::class,
 ];
