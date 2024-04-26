c3261f1249d083611244ea00fe7876408c3653ad
draganb24 <nemanjabjelica45@gmail.com>
2024-04-26 02:46:00 +0200

    local file pciker for tinymce

--- resources/views/shared/tinymce-config.blade.php
+++ resources/views/shared/tinymce-config.blade.php
@@ -2,10 +2,36 @@
 <script>
     tinymce.init({
         selector: 'textarea',
-        plugins: 'advlist autolink lists link image charmap print preview anchor',
-        toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
-        height: 400,
-        license_key: 'gpl'
+        plugins: 'image code advlist autolink lists link image charmap print preview anchor',
+        toolbar: 'undo redo | link image | code  | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent',
+        image_title: true,
+        automatic_uploads: true,
+        file_picker_types: 'image',
+        file_picker_callback: function(cb, value, meta) {
+            var input = document.createElement('input');
+            input.setAttribute('type', 'file');
+            input.setAttribute('accept', 'image/*');
+
+            input.onchange = function() {
+                var file = this.files[0];
+                var reader = new FileReader();
+
+                reader.onload = function() {
+                    var id = 'blobid' + (new Date()).getTime();
+                    var blobCache = tinymce.activeEditor.editorUpload.blobCache;
+                    var base64 = reader.result.split(',')[1];
+                    var blobInfo = blobCache.create(id, file, base64);
+                    blobCache.add(blobInfo);
+
+                    cb(blobInfo.blobUri(), {
+                        title: file.name
+                    });
+                };
+                reader.readAsDataURL(file);
+            };
+
+            input.click();
+        }
     });
 </script>
 <script>
